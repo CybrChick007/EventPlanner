bb3d7726117444c9a7e32e342dd0e616d3878ee0
Lewis Carpenter <lewisbcarpenter_@outlook.com>
2018-12-13 14:01:23 +0000

    basic express web server

---
+++ http.js
@@ -0,0 +1,9 @@
+let express = require("express");
+let app = express();
+
+app.listen(8080, (err) => {
+  if (err) console.error("error starting server", err);
+  else console.log("server started");
+});
+
+app.use("/", express.static("./", {extensions: ["html"]}));
