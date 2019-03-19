a2eb92b8a9508e8ce87bfe652c92b3e8f315d1c4
up861332 <44001029+up861332@users.noreply.github.com>
2019-03-19 12:28:23 +0000

    added reset button functionality

--- public/scripts/createEvent.js
+++ public/scripts/createEvent.js
@@ -1,6 +1,23 @@
 let saveButton = document.getElementById("savebtn");
 saveButton.addEventListener("click", addEvent);
+let resetButton = document.getElementById("delbtn");
+resetButton.addEventListener("click", resetPress);
+let uploadButton = document.getElementById("uploadButton");
+uploadButton.addEventListener("change", upload);
 
 function addEvent(e){
   e.preventDefault();
+}
+
+function resetPress(e){
+  let shoppingList = document.getElementById("shoppingSelect");
+  while (shoppingList.firstChild) {
+    shoppingList.removeChild(shoppingList.firstChild);
+  }
+  document.getElementById("thumbnail").src="images/placeholderThumb.png";
+}
+
+function upload(e){
+  console.log(uploadButton.files);
+  document.getElementById("thumbnail").src=uploadButton.files;
 }
