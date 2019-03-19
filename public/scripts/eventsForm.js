b57c0b25dbb9eda4b3dafa4033c522c1bf54ffe9
up861332 <44001029+up861332@users.noreply.github.com>
2019-03-19 11:38:59 +0000

    added delete shopping item button functionality

--- public/scripts/eventsForm.js
+++ public/scripts/eventsForm.js
@@ -1,5 +1,7 @@
 let addButton = document.getElementById("sladd");
+let delButton = document.getElementById("sldel");
 addButton.addEventListener("click", addToShopping);
+delButton.addEventListener("click", removeFromShopping);
 
 function addToShopping(e){
   let shopping = document.getElementById("shoppingBox");
@@ -10,6 +12,16 @@ function addToShopping(e){
     item.value = shoppingItem;
     item.textContent = shoppingItem;
     list.appendChild(item);
+  }else {
+    alert("Please enter a shopping item");
+  }
+}
+
+function removeFromShopping(e){
+  let sel = document.getElementById("shoppingSelect");
+  if (sel.selectedIndex != -1) {
+    let opt = sel.options[sel.selectedIndex];
+    sel.removeChild(opt);
   }else {
     alert("Please enter a shopping item");
   }
