05325f7b7d3f081c34441a66c19f060d77640a99
up861332 <44001029+up861332@users.noreply.github.com>
2019-03-20 17:27:13 +0000

    Update createEvent.js
    added the addEvent function

--- public/scripts/createEvent.js
+++ public/scripts/createEvent.js
@@ -6,6 +6,29 @@ let resetButton = document.getElementById("delbtn");
 resetButton.addEventListener("click", resetPress);
 
 function addEvent(e){
+  const NAME = document.getElementById("eventNameBox").value;
+  const DATE = document.getElementById("dateBox").value;
+  const ADDONE = document.getElementById("address1Box").value;
+  const ADDTWO = document.getElementById("address2Box").value;
+  const ADDTHREE = document.getElementById("address3Box").value;
+  const POST = document.getElementById("postcodeBox").value;
+  const TYPE = document.getElementById("typeSelect").value;
+  let shoppingList = document.getElementById("shoppingSelect");
+  let items = shoppingList.children;
+  for (let i = 0; i < items.length; i++) {
+    items[i] = items[i].value;
+  }
+  const DRESS = document.getElementById("dressCodeBox").value;
+  const STATUS = document.getElementById("StatusSelect").value;
+  const THUMB = document.getElementById("uploadButton").files[0];//URL.createObjectURL(THUMB)
+  let myEvent = {"eventName" : NAME,
+                "eventAddress" : ADDONE + ", " + ADDTWO + ", " + ADDTHREE,
+                "eventPostcode" : POST,
+                "eventPublic" : STATUS,
+                "eventURLImage" : THUMB,
+                "eventDressCode" : DRESS,
+                "eventType" : TYPE,
+                "shopList" : items;}//missing eventHost userID and something to send the date to
   console.log("hi");
   e.preventDefault();
 }
