15a79afef05e2d7b62bbf3caf2be578efe3a9a86
Matthew Remon <matthewremon@Matthews-MacBook-Pro-8.local>
2019-03-21 11:58:14 +0000

    Sql changes

--- Server/database.sql
+++ Server/database.sql
@@ -4,48 +4,50 @@ use EventPlanner;
 
 create table user(
   userID int auto_increment,
-  email varchar (255) not null unique,
+  email varchar (50) not null unique,
+  Fname varchar (15) 
+  Lname varchar (15)
   PRIMARY KEY (userID)
 );
 
 create table typeEvent(
   typeID int auto_increment,
-  typeName varchar(255) not null,
+  typeName varchar(25) not null,
   PRIMARY KEY(typeID)
 );
 
 create table event(
   eventID int auto_increment,
-  eventName varchar(255) not null,
+  eventName varchar(25) not null,
   eventAddress varchar(255) not null,
   eventPostcode varchar(8) not null,
-  eventDressCode varchar(255) not null,
+  eventDressCode varchar(30) not null,
   eventPublic boolean not null,
-  eventURLImage varchar(255),
+  eventURLImage varchar(20),
   eventType int not null,
   eventHost int not null,
   eventDate datetime not null,
   PRIMARY KEY (eventID),
-  FOREIGN KEY (eventType) REFERENCES typeEvent (typeID),
-  FOREIGN KEY (eventHost) REFERENCES user (userID)
+  FOREIGN KEY (eventType) REFERENCES typeEvent(typeID),
+  FOREIGN KEY (eventHost) REFERENCES user(userID)
 );
 
 create table guestEvent(
   guestUserID int not null,
   guestEventID int not null,
   PRIMARY KEY (guestUserID, guestEventID),
-  FOREIGN KEY (guestUserID) REFERENCES user (userID),
-  FOREIGN KEY (guestEventID) REFERENCES event (eventID)
+  FOREIGN KEY (guestUserID) REFERENCES user(userID),
+  FOREIGN KEY (guestEventID) REFERENCES event(eventID)
 );
 
 create table shoppingListItem(
-  eventItemName varchar(255) not null,
+  eventItemName varchar(15) not null,
   eventID int not null,
   userBringerID int,
   PRIMARY KEY (eventItemName, eventID),
-  FOREIGN KEY (eventID) REFERENCES event (eventID),
-  FOREIGN KEY (userBringerID) REFERENCES user (userID)
+  FOREIGN KEY (eventID) REFERENCES event(eventID),
+  FOREIGN KEY (userBringerID) REFERENCES user(userID)
 );
 
-INSERT INTO type VALUES (1, "Club");
-INSERT INTO type VALUES (2, "House Party");
+INSERT INTO typeEvent VALUES (1, "Club");
+INSERT INTO typeEvent VALUES (2, "House Party");
