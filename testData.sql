39a672053c9956c1adfd52dbb5a8f2275833c028
up861332 <44001029+up861332@users.noreply.github.com>
2019-04-02 20:14:41 +0100

    changed dates of events so that they should appear be at the top for testing purposes

--- testData.sql
+++ testData.sql
@@ -2,6 +2,6 @@ use EventPlanner;
 
 INSERT INTO user (email, Fname, Lname, Age, ContactNumber) VALUES ("test@myport.ac.uk", "testf", "testl", 18, "0781234567");
 
-INSERT INTO event (eventName, eventAddress, eventPostcode, eventDressCode, eventPublic, eventURLImage, eventType, eventHost, eventDate) VALUES ("test event", "one,two,three", "PO1 2UP", "casual", 1, "", 1, 1, '2019-12-31 00:00:00');
+INSERT INTO event (eventName, eventAddress, eventPostcode, eventDressCode, eventPublic, eventURLImage, eventType, eventHost, eventDate) VALUES ("test event", "one,two,three", "PO1 2UP", "casual", 1, "", 1, 1, '1980-12-31 00:00:00');
 
-INSERT INTO event (eventName, eventAddress, eventPostcode, eventDressCode, eventPublic, eventURLImage, eventType, eventHost, eventDate) VALUES ("test event", "one,two,three", "PO1 2UP", "casual", 1, "", 2, 1, '2020-12-31 00:00:00');
+INSERT INTO event (eventName, eventAddress, eventPostcode, eventDressCode, eventPublic, eventURLImage, eventType, eventHost, eventDate) VALUES ("test event", "one,two,three", "PO1 2UP", "casual", 1, "", 2, 1, '1981-12-31 00:00:00');
